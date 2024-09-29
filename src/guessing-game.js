84827aeca1afe59bcce46f13a450e93ab03db2b8
Vitaly <vi19954@gmail.com>
2024-09-29 19:24:00 +0300

    feat: add the solution of task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min,max) {
+        this.min = min
+        this.max = max
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        return Math.ceil(this.min + (this.max - this.min)/2)
     }
 
     lower() {
-
+        this.max = Math.ceil(this.min + (this.max - this.min)/2)
     }
 
     greater() {
-
+        this.min = Math.ceil(this.min + (this.max - this.min)/2)
     }
 }
 
